0dbdad01740aaa577f11042718f1e17a68d4eca2
RobertoPSF <r.filho1@hotmail.com>
2025-10-26 23:49:13 -0300

    chore: removing unused parameters

--- tests/katana_api_test.py
+++ tests/katana_api_test.py
@@ -18,7 +18,7 @@ def test_start_katana_scan(mock_run_pipeline, client):
 
 
 @patch("app.routes.run_katana_pipeline")
-def test_start_katana_scan_missing_domain(mock_run_pipeline, client):
+def test_start_katana_scan_missing_domain(client):
     payload = {"crawl": True}
     res = client.post("/api/scan", json=payload)
     assert res.status_code == 400
@@ -26,7 +26,7 @@ def test_start_katana_scan_missing_domain(mock_run_pipeline, client):
 
 
 @patch("app.routes.run_katana_pipeline")
-def test_start_katana_scan_missing_crawling(mock_run_pipeline, client):
+def test_start_katana_scan_missing_crawling(client):
     payload = {"domain": "example.com"}
     res = client.post("/api/scan", json=payload)
     assert res.status_code == 400
